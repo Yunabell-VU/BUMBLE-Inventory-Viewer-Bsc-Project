958716b816189419e07fd6bf951648386b062f0e
Yunabell-Vu <yuna.chen@student.vu.nl>
2022-02-12 21:03:08 +0100

    attempt to read xml file

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
-<script setup>
-</script>
-
 <template>
   <header>
-    <div class="wrapper">
-      Hello Yuna!
-    </div>
+    <div class="wrapper">Hello Yuna!</div>
   </header>
 
-  <main>
-  </main>
+  <main></main>
 </template>
 
+<script>
+export default {
+  name: "App",
+  methods: {
+    readXml(xmlFile) {
+      var xmlDoc;
+      var xmlhttp;
+
+      if (typeof window.DOMParser != "undefined") {
+        xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", xmlFile, false);
+        if (xmlhttp.overrideMimeType) {
+          xmlhttp.overrideMimeType("text/xml");
+        }
+        xmlhttp.send();
+        xmlDoc = xmlhttp.responseXML;
+      } else {
+        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async = "false";
+        xmlDoc.load(xmlFile);
+      }
+      var tagObj = xmlDoc.getElementsByTagName(
+        "collaborationSessionMetamodel:ModelInventory"
+      );
+      var userValue =
+        tagObj[0].getElementsByTagName("users")[0];
+      return userValue;
+    },
+  },
+  mounted() {
+    console.log(this.readXml("../static/ModelInventory.xml"));
+  },
+};
+</script>
+
 <style lang="scss">
-@import './assets/base.scss';
+@import "./assets/base.scss";
 
 #app {
   max-width: 1280px;
@@ -31,5 +60,4 @@
     background-color: aquamarine;
   }
 }
-
 </style>
